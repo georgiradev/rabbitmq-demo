dc1e2fd1d1fa3295132d043d1332db2e2070eb87
Georgi Radev <grdevv@gmail.com>
2022-03-04 19:56:23 +0200

    changed toString in ApiResponse to include restaurantName

--- src/main/java/com/demo/rabbitMQDemo/publisher/ApiResponse.java
+++ src/main/java/com/demo/rabbitMQDemo/publisher/ApiResponse.java
@@ -41,9 +41,10 @@ public class ApiResponse {
 
     @Override
     public String toString() {
-        return "Current Order: {" +
+        return "Current Oder: {" +
                 "order=" + order +
                 ", orderStatus=" + orderStatus +
+                ", restaurantName='" + restaurantName + '\'' +
                 '}';
     }
 }
